70f1ffce87be84c3dcb4531510d4070f191c4e4c
Styopa1769 <shc-stepan@yandex.ru>
2019-10-25 00:28:31 +0300

    Поменял условие к задаче про вектор

--- lesson-2/homework/vector/Vector.java
+++ lesson-2/homework/vector/Vector.java
@@ -1,24 +1,32 @@
-package lesson2.vector;
-
-public interface Vector {
+public class Vector {
 
     // Длина вектора. Корень из суммы квадратов
-    double length();
+    double length(){
+        
+    }
 
     // метод, вычисляющий скалярное произведение
-    double scalarProduct(Vector vector);
+    double scalarProduct(Vector vector){
+        
+    }
 
     // метод, вычисляющий векторное произведение
-    //для 2D вектора пусть будет псевдо-скаляр, то есть
-    //формула как для 3D, только с нулевой Z координатой
-    Vector crossProduct(Vector vector);
+    Vector crossProduct(Vector vector){
+        
+    }
 
     // Косинус между двумя векторами
-    double cos(Vector vector);
+    double cos(Vector vector){
+        
+    }
 
     // Сумма двух векторов
-    Vector add(Vector vector);
+    Vector add(Vector vector){
+        
+    }
 
     // Разность двух веторов
-    Vector subtract(Vector vector);
+    Vector subtract(Vector vector){
+        
+    }
 }
